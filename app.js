79514af6786ba39a97a0355bbc4a3f6b0d35d82d
mbajoshuac <angelicjoekeyz@gmail.com>
2020-04-05 12:25:43 +0100

    Weather Icon added from skycon

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ window.addEventListener("load", () => {
   let lat;
   let locationTimezone = document.querySelector(".location-timezone");
   let temperatureDegree = document.querySelector(".temperature-degree");
-  let temperatureDescription = document.querySelector(
-    ".temperature-description"
-  );
+  let temperatureDescription = document.querySelector(".temperature-description");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -15,7 +13,9 @@ window.addEventListener("load", () => {
       console.log(lat);
       console.log(long);
 
+      //adding a proxy to make api work on localhost
       let proxy = "https://cors-anywhere.herokuapp.com/";
+      //declaring the api url with the proxy
       const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
       fetch(api)
         .then((response) => {
@@ -23,7 +23,20 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           console.log(data);
+          const { temperature, summary, icon } = data.currently;
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+          var tt = setIcons(icon, document.querySelector(".icon"));
+          console.log(tt);
         });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
